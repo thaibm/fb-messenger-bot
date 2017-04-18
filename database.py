ff639f241f2283461c53441501b094923081c238
thaibm <thaibm.uet@gmail.com>
2017-04-18 14:55:36 +0700

    add database

--- database.py
+++ database.py
@@ -1,29 +1,29 @@
-# from flask import Flask
-# from flask.ext.sqlalchemy import SQLAlchemy
-# import os
-# import datetime
-#
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-# db = SQLAlchemy(app)
-#
-# class Book(db.Model):
-#     __tablename__ = 'book'
-#     id = db.Column(db.Integer, primary_key = True)
-#     name = db.Column(db.String(255))
-#     author = db.Column(db.String(255))
-#     category = db.Column(db.String(255))
-#     url = db.Column(db.String(255))
-#     description = db.Column(db.String(255))
-#     created_at = db.Column(db.Date, default=datetime.datetime.now())
-#     updated_at = db.Column(db.Date, onupdate=datetime.datetime.now())
-#
-#     def __init__(self, name, author, category, url, description):
-#         self.name = name
-#         self. author = author
-#         self.category = category
-#         self.url = url
-#         self.description = description
-#
-#     def __repr__(self):
-#         return "Book %r" % self.name
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+import os
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    __tablename__ = 'book'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(255))
+    author = db.Column(db.String(255))
+    category = db.Column(db.String(255))
+    url = db.Column(db.String(255))
+    description = db.Column(db.String(255))
+    created_at = db.Column(db.Date, default=datetime.datetime.now())
+    updated_at = db.Column(db.Date, onupdate=datetime.datetime.now())
+
+    def __init__(self, name, author, category, url, description):
+        self.name = name
+        self. author = author
+        self.category = category
+        self.url = url
+        self.description = description
+
+    def __repr__(self):
+        return "Book %r" % self.name
