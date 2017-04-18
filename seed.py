58cb24fd7eae156b8eff6fb7144e02135d4d2030
thaibm <thaibm.uet@gmail.com>
2017-04-18 21:09:23 +0700

    Fig bugs

--- seed.py
+++ seed.py
@@ -6,9 +6,9 @@ class Seed:
         db.create_all()
 
     def make(self):
-        with open("data/tiki-full.csv", "r", encoding="utf8") as file:
+        with open('data/tiki-full.csv', 'r', encoding='utf8') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                book = Book(row['name'], row['author'], row["categories"], row["url"], row["description"])
+                book = Book(row['name'], row['author'], row['categories'], row['url'], row['description'])
                 db.session.add(book)
                 db.session.commit()
