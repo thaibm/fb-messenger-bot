f10a77bce1bbb1526ccb4af666ab3da0b1343918
thaibm <thaibm.uet@gmail.com>
2017-04-20 07:36:50 +0700

    test seed

--- seed.py
+++ seed.py
@@ -6,10 +6,10 @@ class Seed:
         db.create_all()
 
     def make(self):
-        with open('data/test.csv', 'r') as file:
+        with open('data/tiki-full.csv', 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                print (row)
-                # book = Book(row['name'], row['author'], row['categories'], row['url'], row['description'])
-                # db.session.add(book)
-                # db.session.commit()
+                print (row['name'], row['author'])
+                book = Book(row['name'], row['author'], row['categories'], row['url'], row['description'])
+                db.session.add(book)
+                db.session.commit()
