01bf99176492099fa8bfaa353c139225dea92c55
thaibm <thaibm.uet@gmail.com>
2017-04-20 15:31:46 +0700

    correct data

--- seed.py
+++ seed.py
@@ -10,6 +10,6 @@ class Seed:
             reader = csv.reader(file)
             for row in reader:
                 print(row[2].strip())
-                book = Book(row[2], row[4], row[3], row[0], row[1])
+                book = Book(row[2].strip(), row[4].strip(), row[3].strip(), row[0].strip(), row[1].strip())
                 db.session.add(book)
                 db.session.commit()
