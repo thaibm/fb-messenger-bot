1e3cf776c83374cb14c1fb39510c50620cd02b98
thaibm <thaibm.uet@gmail.com>
2017-04-22 18:21:35 +0700

    Fix bug

--- recieve_message.py
+++ recieve_message.py
@@ -41,7 +41,6 @@ def recieve(data):
 
 def send_message(recipient_id, message_text):
 
-    print(message_text)
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
