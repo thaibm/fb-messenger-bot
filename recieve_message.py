43532695b3bb3441416358c4fc48f90dd18868c4
thaibm <thaibm.uet@gmail.com>
2017-04-22 19:12:53 +0700

    Fix bug

--- recieve_message.py
+++ recieve_message.py
@@ -24,9 +24,9 @@ def recieve(data):
                     message_text = messaging_event["message"][
                         "text"]  # the message's text
 
-                    # book = BookRecord.get(1)
+                    book = BookRecord.get(1)
                     # book = Book("Nhà giả kim", "Paulo", "", "", "")
-                    send_message(sender_id, message_text)
+                    send_message(sender_id, book.name)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
