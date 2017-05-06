6e4e60be690db14b37cdd3a67fa0d97d391b9e54
thaibm <thaibm.uet@gmail.com>
2017-05-06 15:18:53 +0700

    install numpy

--- recieve_message.py
+++ recieve_message.py
@@ -25,8 +25,8 @@ def recieve(data):
                     message_text = messaging_event["message"][
                         "text"]  # the message's text
 
-                    k_doc = KNN(5, message_text)
-                    book = BookRecord.get(k_doc[0][0])
+                    # k_doc = KNN(5, message_text)
+                    book = BookRecord.get(1)
                     send_message(sender_id, book.name)
 
                 if messaging_event.get("delivery"):  # delivery confirmation
