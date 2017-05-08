724374cb6c76731754f924e645048348d60334bf
thaibm <thaibm.uet@gmail.com>
2017-05-09 00:13:14 +0700

    Correct data

--- wmd/wmd.py
+++ wmd/wmd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
-
+# import gensim, glob, os
 import math
 import operator
 import re
@@ -12,7 +12,7 @@ import numpy as np
 from pyemd import emd
 from sklearn.metrics import euclidean_distances
 
-# wv = gensim.models.KeyedVectors.load_word2vec_format("../nlp/word2vec/vector.txt",
+# wv = gensim.models.KeyedVectors.load_word2vec_format("../nlp/word2vec/vector",
 #                                                      binary=False)
 # shape = wv.syn0.shape
 # if not os.path.exists("data/embed_vn.dat"):
@@ -20,32 +20,78 @@ from sklearn.metrics import euclidean_distances
 #
 #     fp = np.memmap("data/embed_vn.dat", dtype=np.double, mode='w+', shape=shape)
 #     fp[:] = wv.syn0[:]
-#     with open("data/embed_vn.dat.vocab", "w", encoding='utf-8') as f:
+#     with open("data/embed_vn.vocab", "w", encoding='utf-8') as f:
 #         for _, w in sorted((voc.index, word) for word, voc in wv.vocab.items()):
 #             print(w, file=f)
 #     del fp, wv
 
-shape = (11967, 300)
-path = "wmd/"
+shape = (11832, 300)
+# path = "wmd/"
+path = ""
 W = np.memmap(path+"data/embed_vn.dat", dtype=np.double, mode="r", shape=shape)
 
 with open(path+"data/embed_vn.vocab", encoding='utf-8') as f:
     vocab_list = map(str.strip, f.readlines())
 vocab_dict = {w: k for k, w in enumerate(vocab_list)}
 
+
+def get_xd(document):
+    # Matrix of document
+    ds = document.split()
+    list_doc = [word for word in ds if word in vocab_dict]
+    vect = Counter(list_doc)
+
+    input_vector_matrix = W[[vocab_dict[w] for w in vect.keys()]]
+    # Calculate word frequency
+    v = list(vect.values())
+    v = np.ravel(v)
+    frequency = np.divide(v, v.sum())
+    # Calculate di*xi
+    input_vector = []
+    for i in range(0, len(input_vector_matrix)):
+        input_vector.append(
+            np.multiply(input_vector_matrix[i], frequency[i]))
+
+    X = np.sum(input_vector, axis=0)
+    return X
+
+# if not os.path.exists("data/Xd.dat"):
+#     print("Caculate Xd.dat...")
+#
+#     list_docs = []
+#     for filename in glob.glob(os.path.join("../nlp/data-filter/result/", '*.txt')):
+#         for line in open(filename, encoding='utf-8'):
+#             line = line.strip()
+#             if line != '':
+#                 list_docs.append(line)
+#     # list_docs = sorted(list_docs)
+#     # print(list_docs[10])
+#     X_dict = []
+#     for i in range(0, len(list_docs)):
+#         X_dict.append(get_xd(list_docs[i]))
+#     fp = np.memmap("data/Xd.dat", dtype=np.double, mode='w+',
+#                    shape=(len(list_docs), 300))
+#     fp[:] = X_dict[:]
+#
+#     with open("data/list_doc.vocab", "w", encoding='utf-8') as f:
+#         for doc in list_docs:
+#             print(doc, file=f)
+#     del fp
+
 with open(path+"data/list_doc.vocab", encoding='utf-8') as f:
     list_docs = f.read().splitlines()
 doc_dict = {doc: k for k, doc in enumerate(list_docs)}
+
+x_matrix = np.memmap(path+"data/Xd.dat", dtype=np.double, mode="r",
+                     shape=(len(list_docs), 300))
+
 # Get stop-words
 SW = set()
-for line in open(path+'vn_stopword.txt'):
+for line in open(path + 'vn_stopword.txt'):
     line = line.strip()
     if line != '':
         SW.add(line)
 stop_words = list(SW)
-
-x_matrix = np.memmap(path+"data/Xd.dat", dtype=np.double, mode="r",
-                     shape=(len(list_docs), 300))
 
 
 def WMD(docs_1, docs_2):
@@ -93,56 +139,8 @@ def WMD(docs_1, docs_2):
     return _emd
 
 
-def get_xd(document):
-    # Matrix of document
-    ds = document.split()
-    list_doc = [word for word in ds if word in vocab_dict]
-    vect = Counter(list_doc)
-
-    input_vector_matrix = W[[vocab_dict[w] for w in vect.keys()]]
-    # Calculate word frequency
-    v = list(vect.values())
-    v = np.ravel(v)
-    frequency = np.divide(v, v.sum())
-    # Calculate di*xi
-    input_vector = []
-    for i in range(0, len(input_vector_matrix)):
-        input_vector.append(
-            np.multiply(input_vector_matrix[i], frequency[i]))
-
-    X = np.sum(input_vector, axis=0)
-    return X
-
-
 def WCD(document):
     x1 = get_xd(document)
-
-    # if not os.path.exists("data/Xd.dat"):
-    #     print("Caculate Xd.dat...")
-    #
-    #     list_docs = []
-    #     path = "../nlp/data-filter/result/"
-    #     for filename in glob.glob(os.path.join(path, '*.txt')):
-    #         for line in open(filename, encoding='utf-8'):
-    #             line = line.strip()
-    #             if line != '':
-    #                 list_docs.append(line)
-    #     # list_docs = sorted(list_docs)
-    #     # print(list_docs[10])
-    #     X_dict = []
-    #     for i in range(0, len(list_docs)):
-    #         X_dict.append(get_xd(list_docs[i]))
-    #     fp = np.memmap("data/Xd.dat", dtype=np.double, mode='w+',
-    #                    shape=(len(list_docs), 300))
-    #     fp[:] = X_dict[:]
-    #
-    #     with open("data/list_doc.vocab", "w", encoding='utf-8') as f:
-    #         for doc in list_docs:
-    #             print(doc, file=f)
-    #         del fp
-
-    # with open("data/list_doc.vocab", encoding='utf-8') as f:
-    #     doc_list = map(str.strip, f.readlines())
 
     temple = np.matrix(x_matrix) * (np.matrix(x1).transpose())
     results = {}
@@ -177,6 +175,8 @@ def __rwmd(docs_1, docs_2):
     v2 = np.ravel(v2)
     v2 = np.divide(v2, v2.sum())
 
+    print(matrix_1)
+    print(matrix_2)
     d1_ = euclidean_distances(matrix_1, matrix_2)
     d1_min = np.amin(d1_, axis=1)
 
@@ -197,10 +197,12 @@ def knn(k, input_doc):
     result = re.sub('[%s]' % punctuation, ' ', data_lower)
     # result = " ".join([word for word in result.split() if word not in stop_words])
     print(result)
+
     wcd = WCD(result)
 
     wmd_k_doc = {}
     count = 1
+    # print(list_docs[wcd[0][0]])
     max_rwmd = __rwmd(list_docs[wcd[0][0]], input_doc)
     for i in range(0, len(wcd)):
         if count <= k:
@@ -219,11 +221,12 @@ def knn(k, input_doc):
 
 
 def main():
-    doc_1 = "Cẩm nang để phát huy tối đa tiềm năng của những đứa trẻ ""cứng đầu"",Thật dễ dàng để có thể nhận ra đâu là một đứa trẻ cứng đầu."
+    doc_1 = "HOÀNG TỬ BÉ"
 
     doc_2 = "Cha mẹ nào cũng mong muốn con mình có được tương lai hạnh phúc. Thời kì học sinh Tiểu học là giai đoạn quan trọng tạo cho trẻ nền tảng hạnh phúc sau này, bởi nó đánh dấu bước trưởng thành đầu tiên của trẻ trong cuộc đời."
     t1 = time.time()
-    print(knn(10, doc_1))
+    print(knn(5, doc_1))
+    # print(__rwmd(doc_1, doc_2))
     print(time.time() - t1)
 
 
